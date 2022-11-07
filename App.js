f3be771684c83dd34b442f6ded545e1d529822e5
Tandin Dorji <65913825+tandynhd@users.noreply.github.com>
2022-11-07 22:45:55 +0700

    updates

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 import { Button } from 'react-native';
 import DatePicker from 'react-native-date-picker';
 import PushNotification from 'react-native-push-notification';
-import Notifications from './Notifications';
-import AppBar from './AppBar';
-import FooterBar from './FooterBar';
-import SelCat from './SelectCat';
+import Notifications from './components/Notifications';
+import AppBar from './components/AppBar';
+import FooterBar from './components/FooterBar';
+import SelCat from './components/SelectCat';
 import { TextInput } from 'react-native-paper';
 
 import {
@@ -42,35 +42,43 @@ export default function Ginya() {
 
   return (
     <>
-    <AppBar />
-    <View style={{ padding: 10}}>
-          <TextInput
-            label="Task Name"
-            left={<TextInput.Icon name="account" />}
-            mode="outlined"
-            style={{ margin: 10 }}
-          />
-
-          <TextInput
-              label="Task Description"
-              left={<TextInput.Icon name="account" />}
-              mode="outlined"
-              multiline
-              style={{ margin: 10 }}
-            />
-
-        <View style={styles.container2}>
-            <SelCat />
-        </View>
-
-        <View style={styles.container} >
-             <Text variant="titleLarge">Choose Medication Time</Text>
-             <DatePicker date={date} onDateChange={setDate} />
-             <View style={styles.wrapper} />
-             <Button title="Set notification" onPress={setNotification} />
-        </View>
+    <View>
+        <AppBar />
     </View>
-    <FooterBar />
+    <View style = {{flex:3}}>
+        <ScrollView>
+            <View style={{ padding: 10}}>
+                  <TextInput
+                    label="Task Name"
+                    left={<TextInput.Icon name="account" />}
+                    mode="outlined"
+                    style={{ margin: 10 }}
+                  />
+
+                  <TextInput
+                      label="Task Description"
+                      left={<TextInput.Icon name="account" />}
+                      mode="outlined"
+                      multiline
+                      style={{ margin: 10 }}
+                    />
+
+                  <View style={styles.container2}>
+                                  <SelCat />
+                              </View>
+
+                  <View style={styles.container} >
+                                   <Text variant="titleLarge">Choose Medication Time</Text>
+                                   <DatePicker date={date} onDateChange={setDate} />
+                                   <Button title="Set notification" onPress={setNotification} />
+                              </View>
+
+            </View>
+        </ScrollView>
+        </View>
+    <View style = {{flex:1}}>
+        <FooterBar />
+    </View>
     </>
   );
 };
