8303474d2190f0317f2c535eea5665a66a2f53ab
6222790345 <6222790345@g.siit.tu.ac.th>
2022-11-15 00:23:10 +0700

    Calendar Page Dots

--- pages/Atoms.js
+++ pages/Atoms.js
@@ -36,54 +36,54 @@ export const tasksList = atom({
 export const tasksStatus = atom({
   key: "tasksStatus",
   default: [
-             {
-               "date": "2022-11-01",
-               "color": "red"
-             },
-             {
-                "date": "2022-11-02",
-                "color": "orange"
-              },
-             {
-                "date": "2022-11-03",
-                "color": "red"
-              },
-              {
-                 "date": "2022-11-04",
-                 "color": "green"
-               },
-               {
-                  "date": "2022-11-05",
-                  "color": "orange"
-                },
-               {
-                  "date": "2022-11-06",
-                  "color": "green"
-                },
-                {
-                   "date": "2022-11-07",
-                   "color": "green"
-                 },
-                 {
-                    "date": "2022-11-08",
-                    "color": "orange"
-                  },
-                 {
-                    "date": "2022-11-09",
-                    "color": "red"
-                  },
-                  {
-                     "date": "2022-11-10",
-                     "color": "green"
-                   },
-                   {
-                      "date": "2022-11-11",
-                      "color": "orange"
-                    },
-                   {
-                      "date": "2022-11-12",
-                      "color": "green"
-                    },
+     {
+       "date": "2022-11-01",
+       "color": "red"
+     },
+     {
+        "date": "2022-11-02",
+        "color": "orange"
+      },
+     {
+        "date": "2022-11-03",
+        "color": "red"
+      },
+      {
+         "date": "2022-11-04",
+         "color": "green"
+       },
+       {
+          "date": "2022-11-05",
+          "color": "orange"
+        },
+       {
+          "date": "2022-11-06",
+          "color": "green"
+        },
+        {
+           "date": "2022-11-07",
+           "color": "green"
+         },
+         {
+            "date": "2022-11-08",
+            "color": "orange"
+          },
+         {
+            "date": "2022-11-09",
+            "color": "red"
+          },
+          {
+             "date": "2022-11-10",
+             "color": "green"
+           },
+           {
+              "date": "2022-11-11",
+              "color": "orange"
+            },
+           {
+              "date": "2022-11-12",
+              "color": "green"
+            },
 
-           ]
+   ]
 });
