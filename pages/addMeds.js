0a86a6e16b0634699e47b5ea68fe5f5a6ed43d0a
6222790345 <6222790345@g.siit.tu.ac.th>
2022-11-15 00:39:23 +0700

    AddMeds Components Put back in

--- pages/addMeds.js
+++ pages/addMeds.js
@@ -39,8 +39,7 @@ export default function AddMedsPage() {
   const [taskCategory, setTaskCategory] = useState("Eye");
   const [taskPriority, setTaskPriority] = useState("Medium");
   const [taskList, setTaskList] = useRecoilState(tasksList);
-
-
+  const [active, setActive] = useState("1");
 
   const setNotification = () => {
     Notifications.scheduleNotification(date, taskName, taskDesc);
@@ -93,13 +92,69 @@ export default function AddMedsPage() {
                       value = {taskDesc}
                     />
             </View>
-            <View style={styles.container} >
+            <View style={styles.container1} >
                 <Text style={{color: 'black', fontWeight: 'bold', padding: 5, textAlign: 'left' }}>Choose Medication Time</Text>
-                <DatePicker style={{borderRadius: 25}} mode="datetime" date={date} onDateChange={setDate} />
+                <DatePicker mode="datetime" date={date} onDateChange={setDate} />
             </View>
-            <SelRep />
-            <SelCat />
-            <TaskPri />
+            <View style={styles.container} name="Repeat Notification">
+                <Text style={{color: 'black', fontWeight: 'bold', padding: 5}}>Repeat Notification Every</Text>
+                <View style={{ flexDirection:"row", alignItems:"space-around", flexWrap: "wrap" }}>
+                    <Button style={active=="1"?styles.buttonActive:styles.button} title="day" mode='elevated' textColor={active=="1"?"black":"white"} onPress={() => setActive("1")}>
+                        Day
+                    </Button>
+                    <Button style={active=="2"?styles.buttonActive:styles.button} title="week" mode="elevated" textColor={active=="2"?"black":"white"} onPress={() => setActive("2")}>
+                        Week
+                    </Button>
+                    <Button style={active=="3"?styles.buttonActive:styles.button} title="month" mode="elevated" textColor={active=="3"?"black":"white"} onPress={() => setActive("3")}>
+                        Month
+                    </Button>
+                    <Button style={active=="4"?styles.buttonActive:styles.button} title="+" mode="elevated" textColor={active=="4"?"black":"white"} onPress={() => setActive("4")}>
+                       +
+                    </Button>
+                </View>
+            </View>
+            <View style={styles.container} name="Select Category">
+                <Text style={{color: 'black', fontWeight: 'bold', padding: 5}}>Medication Category</Text>
+                <View style={{ flexDirection:"row", alignItems:"space-around", flexWrap: "wrap"}}>
+                    <Button style={active=="1"?styles.buttonActive:styles.button} title="Eye" icon="eye" mode='elevated' textColor={active=="1"?"black":"white"} onPress={() => setActive("1")}>
+                        Eye
+                    </Button>
+                    <Button style={active=="2"?styles.buttonActive:styles.button} title="Ear" icon="volume-high" mode="elevated" textColor={active=="2"?"black":"white"} onPress={() => setActive("2")}>
+                        Ear
+                    </Button>
+                    <Button style={active=="3"?styles.buttonActive:styles.button} title="Head" icon="head-flash" mode="elevated" textColor={active=="3"?"black":"white"} onPress={() => setActive("3")}>
+                        Head
+                    </Button>
+                    <Button style={active=="4"?styles.buttonActive:styles.button} title="Ankles" icon="foot-print" mode="elevated" textColor={active=="4"?"black":"white"} onPress={() => setActive("4")}>
+                       Ankles
+                    </Button>
+                    <Button style={active=="5"?styles.buttonActive:styles.button} title="Hip" icon="account" mode="elevated" textColor={active=="5"?"black":"white"} onPress={() => setActive("5")}>
+                       Hip
+                    </Button>
+                    <Button style={active=="6"?styles.buttonActive:styles.button} title="+" mode='elevated' textColor={active=="6"?"black":"white"} onPress={() => setActive("6")}>
+                       +
+                    </Button>
+                </View>
+            </View>
+            <View style={styles.container} name="Task Priority">
+                <Text style={{color: 'black', fontWeight: 'bold', padding: 5}}>
+                    Task Priority
+                </Text>
+                <View style={{ flexDirection:"row", alignItems:"space-around", flexWrap: "wrap"}}>
+                    <Button style={active=="1"?styles.buttonActive:styles.button} title="low" mode='contained-tonal' textColor={active=="1"?"black":"white"} onPress={() => setActive("1")}>
+                        Low
+                    </Button>
+                    <Button style={active=="2"?styles.buttonActive:styles.button} title="mid" mode="elevated" textColor={active=="2"?"black":"white"} onPress={() => setActive("2")}>
+                        Mid
+                    </Button>
+                    <Button style={active=="3"?styles.buttonActive:styles.button} title="high" mode="elevated" textColor={active=="3"?"black":"white"} onPress={() => setActive("3")}>
+                        High
+                    </Button>
+                    <Button style={active=="4"?styles.buttonActive:styles.button} title="+" mode="elevated" textColor={active=="4"?"black":"white"} onPress={() => setActive("4")}>
+                       +
+                    </Button>
+                </View>
+            </View>
             <Button style={{margin: 5, backgroundColor: "lightskyblue"}} textColor="black" fontWeight="bold" title="Set notification" mode="elevated" onPress={setNotification}> Set Notification </Button>
         </ScrollView>
         </View>
@@ -108,7 +163,7 @@ export default function AddMedsPage() {
 };
 
 const styles = StyleSheet.create({
-    container: {
+    container1: {
         justifyContent: 'center',
         alignItems: 'center',
         padding: 5
@@ -119,5 +174,35 @@ const styles = StyleSheet.create({
     },
     wrapper: {
         height: 60,
+    },
+    container: {
+        flex:1,
+        marginVertical: 5, marginHorizontal: 10,
+        backgroundColor: '#dcdcdc',
+        borderRadius: 10,
+        padding: 5,
+        elevation: 5
+    },
+    button: {
+        paddingHorizontal: 1,
+        paddingVertical: 0,
+        borderRadius: 10,
+        backgroundColor: "#979797",
+        alignSelf: "flex-start",
+        marginHorizontal: "1%",
+        marginBottom: 6,
+        minWidth: "10%",
+        textAlign: "left",
+    },
+     buttonActive: {
+        paddingHorizontal: 1,
+        paddingVertical: 0,
+        borderRadius: 10,
+        backgroundColor: "lightskyblue",
+        alignSelf: "flex-start",
+        marginHorizontal: "1%",
+        marginBottom: 6,
+        minWidth: "10%",
+        textAlign: "left",
     }
 })
